f42709410ea56e852d4eb3e7ff0128c7ff7147d9
thopd-vnpay <phuctho.phuctho@gmail.com>
2020-07-20 22:27:28 +0700

    create angular

---
+++ src/app/fake-movies.ts
@@ -0,0 +1,114 @@
+import {Movie} from "./_model/movie";
+
+export var fakeMovies: Movie[] = [
+  {
+    id: 1,
+    name: "The Ghost and the Darkness",
+    year: 1996
+  },
+  {
+    id: 2,
+    name: "The Blind Side",
+    year: 2009
+  },
+  {
+    id: 3,
+    name: "Star Wars",
+    year: 1977
+  },
+  {
+    id: 4,
+    name: "Back to the Future",
+    year: 1985
+  },
+  {
+    id: 5,
+    name: "The Matrix",
+    year: 1999
+  },
+  {
+    id: 6,
+    name: "Inception",
+    year: 2010
+  },
+  {
+    id: 7,
+    name: "Freedom Writers",
+    year: 2007
+  },
+  {
+    id: 8,
+    name: "Appolo 13",
+    year: 1995
+  },
+  {
+    id: 9,
+    name: "Lone Survivor",
+    year: 2013
+  },
+  {
+    id: 10,
+    name: "All the President's Men",
+    year: 1976
+  },
+  {
+    id: 11,
+    name: "Walk the Line",
+    year: 2005
+  },
+  {
+    id: 12,
+    name: "Catch Me If You Can",
+    year: 2002
+  },
+  {
+    id: 13,
+    name: "Gorillas in the Mist",
+    year: 1988
+  },
+  {
+    id: 14,
+    name: "Girl, Interrupted",
+    year: 1999
+  },
+  {
+    id: 15,
+    name: "What's Love Got to Do with It",
+    year: 1993
+  },
+  {
+    id: 16,
+    name: "Mommie Dearest",
+    year: 1981
+  },
+  {
+    id: 17,
+    name: "The Pianist",
+    year: 2003
+  },
+  {
+    id: 18,
+    name: "The Impossible ",
+    year: 2012
+  },
+  {
+    id: 19,
+    name: "Coal Miner's Daughter",
+    year: 1980
+  },
+  {
+    id: 20,
+    name: "Invincible",
+    year: 2006
+  },
+  {
+    id: 21,
+    name: "The Boy in the Plastic Bubble",
+    year: 1976
+  },
+  {
+    id: 22,
+    name: "The Amityville Horror",
+    year: 1979
+  }
+];
---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Movie} from "../_model/movie";
+import {fakeMovies} from "../fake-movies";
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.scss']
+})
+export class MovieComponent implements OnInit {
+  movie: Movie = {
+    id: 1,
+    name: "fsfssss",
+    year: 1234
+  }
+  movies = fakeMovies;
+    constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
