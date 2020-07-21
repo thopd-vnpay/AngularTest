cb3bef214bf2fa4fa16e69086f7bfc3111d5306e
thopd-vnpay <phuctho.phuctho@gmail.com>
2020-07-21 21:26:00 +0700

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieComponent } from './movie/movie.component';
 import { FormsModule } from "@angular/forms";
+import { XyzComponent } from './xyz/xyz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieComponent
+    MovieComponent,
+    XyzComponent
   ],
   imports: [
     FormsModule,
